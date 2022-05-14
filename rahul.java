54272787737138180946363ef2a9da0ae34dca0a
devopsaneeskdp <aneesahamed922@gmail.com>
2022-05-14 14:29:53 +0000

    updated rahul

--- rahul.java
+++ rahul.java
@@ -3,3 +3,5 @@ the institution of the body
 the main targets should be 
 karnataka,telangana,mp,rajastan,chattisgarh
 before the general election
+"the structure of the congress should be completely revamped"
+rahul gandhi is dedicated to perform this task
