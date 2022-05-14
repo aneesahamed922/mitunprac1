a46ae773cc4b555d9e2c345a88707916f20923a8
devopsaneeskdp <aneesahamed922@gmail.com>
2022-05-14 14:50:50 +0000

    updated

--- rahul.java
+++ rahul.java
@@ -5,3 +5,7 @@ karnataka,telangana,mp,rajastan,chattisgarh
 before the general election
 "the structure of the congress should be completely revamped"
 rahul gandhi is dedicated to perform this task
+the congress main 3 targets
+1. more youth into the congress
+2. retire old guard
+3. aggresively defend the spirit of secularism
